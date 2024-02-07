f105d1c6e6ec04de2e84b05a6db68538d8676117
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-02-07 11:08:56 -0400

    feat: API routes

---
+++ src/app/api/cadastro/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+/**
+ * @description
+ * Controller que será executada quando um POST na rota /api/cadastro for executado
+ */
+export async function POST(request: NextRequest, response: NextResponse) {}
---
+++ src/app/api/point/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest, NextResponse } from "next/server";
+/**
+ * @description
+ * Controller que será executada quando um POST na rota /api/point for login
+ */
+export async function POST(request: NextRequest, response: NextResponse) {}
