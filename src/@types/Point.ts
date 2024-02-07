0d1465c6dd83938f3a7987fdcc0307ad713f4745
FELIPE_AGUIAR <felipeferreiraaguiar9@gmail.com>
2024-02-07 01:10:22 -0400

    feat: Features base types declaration

---
+++ src/@types/Point.ts
@@ -0,0 +1,36 @@
+export interface ISheetIntegration {
+  tutorEmail: string;
+  associatedStudentsEmail: string[];
+  googleSheetsLink: string;
+}
+
+/**
+ * @description Esta interface representa a entidade `Point` no banco de dados.
+ *
+ * Este é o modelo que representa cada documento da coleção `Points`
+ */
+export interface IPoint {
+  pointId: string;
+  userEmail: string;
+  observation: string | null;
+  joinedAt: Date;
+  leftAt: Date | null;
+}
+
+/**
+ * @description Dados necessários para se criar um novo documento na coleção `Points`
+ *
+ * Segue a ilustração de **UMA DAS MANEIRAS** de se usar esta interface:
+ *
+ * ```
+ * function baterPonto({ userEmail, observation, joinedAt }: CreatePointInput) {
+ * // Realizar verificações
+ * // Inserir no banco de dados
+ * }
+ * ```
+ */
+export interface CreatePointInput {
+  userEmail: string;
+  observation: string | null;
+  joinedAt: Date;
+}
---
+++ src/@types/User.ts
@@ -0,0 +1,36 @@
+import { IPoint } from "./Point";
+
+/**
+ * @description Esta interface representa a entidade `User` no banco de dados.
+ *
+ * Este é o modelo que representa cada documento da coleção `Users`
+ *
+ * Em NoSQL (não relacional), coleções = tabelas, documentos = linhas
+ *
+ */
+export interface IUser {
+  userId: string;
+  email: string;
+  name: string;
+  password: string;
+  pointRegisters: IPoint[];
+  createdAt: Date;
+}
+
+/**
+ * @description Dados necessários para se criar um novo usuário
+ *
+ * Segue a ilustração de **UMA DAS MANEIRAS** de se usar esta interface:
+ *
+ * ```
+ * function criarUsuario({ email, name, password }:CreateUserInput) {
+ * // Realizar verificações
+ * // Inserir no banco de dados
+ * }
+ * ```
+ */
+export interface CreateUserInput {
+  email: string;
+  name: string;
+  password: string;
+}
